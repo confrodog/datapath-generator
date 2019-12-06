01d762765cc19d1cc4d491f7b6f583e64b080088
alex <alexanderfreyes@email.arizona.edu>
2019-12-05 18:15:06 -0700

    Adding in the parent and children finding. I think it's working so far..

--- fds.cpp
+++ fds.cpp
@@ -70,6 +70,7 @@ public:
     Operation operation; // add/sub, mult, logic, div/mod
     vector<string> inputs; // max 2 inputs, pretty sure it has to be this way
     Node *parent;
+    vector<Node*> children;
     string output; // max 1 output
     int asap; // this is the first element of timeframe for FDS
     int alap; // this is the second element of timeframe for FDS
@@ -77,7 +78,9 @@ public:
     bool scheduled; // default false, helps to shrink each iteration of FDS
     int selfForce; // SUM(DG(i) * deltaX(i)) where DG(i) is time probability and deltaX(i) is
     Node(){
-        this->parent = Node();
+        this->parent = NULL;
+        this->children = {};
+        this->inputs = {};
         this->operation = Operation();
         this->output = "";
         this->alap = 0;
@@ -90,7 +93,9 @@ public:
 
 class Graph{ // will contain the vector of all the edges, where the edges have the children/parent information
     public:
-    vector<Node> vertices;
+    vector<Node> vertices; //a list of all nodes in this graph
+    Node noop;
+    Node sink; //I'm thinking these might be helpful
     Graph(){
     }
 };
@@ -177,25 +182,99 @@ void FDS(Graph g, int lambda){
 }
 
 int main() {
-    cout << "checking to make sure I can build this correctly";
-    Variable var1("x", "int", 2);
-    Variable var2("y", "int", 2);
-    Variable var3("z", "int", 2);
-    Operation op1;
-    op1.result = var3;
-    op1.equals = "=";
-    op1.var1 = var1;
-    op1.var2 = var2;
+    cout << "checking to make sure I can build this correctly\n";
+    vector<Operation> listOfOps;
 
-//     Variable result; // the leftmost variable aka result of the op in most cases
-//     string equals; // the equals operator
-//     Variable var1; // the first variable after the equals sign
-//     Variable var2; // the second variable after the equals sign
-//     Variable var3; // the third variable after the equals sign
-//     string op1; // the first operator
-//     string op2; // the second operator
-//     string name; // name of the operation
-//     string type; // type (s for signed, u for unsigned)
-//     int bits; // bits 1, 2, 8, 16, 32, 64
+    Variable w("w", "int", 2);
+    Variable x("x", "int", 2);
+    Variable x1("x1", "int", 2);
+    Variable y("y", "int", 2);
+    Variable z("z", "int", 2);
+    Variable z2("z", "int", 2);
+
+    //opp names add/sub, mult, logic, div/mod
+    Operation op1; // z = x + y
+    op1.result = z;
+    op1.equals = "=";
+    op1.var1 = x;
+    op1.var2 = y;
+    op1.op1 = "+";
+    op1.bits = 2;
+    op1.type = "s";
+    op1.name = "add/sub";
+
+    listOfOps.push_back(op1);
+
+    Operation op2; //x1 = x-w
+    op2.result = x1;
+    op2.equals = "=";
+    op2.var1 = x;
+    op2.var2 = w;
+    op2.op1 = "-";
+    op2.bits = 2;
+    op2.type = "s";
+    op2.name = "add/sub";
+    
+    listOfOps.push_back(op2);
+
+    Operation op3; //w = z*x1
+    op3.result = w;
+    op3.equals = "=";
+    op3.var1 = z;
+    op3.var2 = x1;
+    op3.op1 = "*";
+    op3.bits = 4;
+    op3.type = "s";
+    op3.name = "mult";
+
+    listOfOps.push_back(op3);
+
+    Operation op4; //z2 = w/z
+    op4.result = z2;
+    op4.equals = "=";
+    op4.var1 = w;
+    op4.var2 = z;
+    op4.op1 = "/";
+    op4.bits = 4;
+    op4.type = "s";
+    op4.name = "div/mod";
+
+    listOfOps.push_back(op4);
+
+    Graph g;
+    //creating all of the nodes for the graph
+    for(int i = 0; i < listOfOps.size(); i++) {
+        Node curr;
+        Operation currentOp = listOfOps.at(i);
+        curr.operation = currentOp;
+        curr.inputs.push_back(currentOp.var1.name);
+        curr.inputs.push_back(currentOp.var2.name);
+        curr.output = currentOp.result.name;
+        g.vertices.push_back(curr);
+    }
+
+    vector<Node> visitedNodes = {}; //keep track of what nodes have been seen
+    vector<string> visistedOutputs = {};
+    for(int i = 0; i < g.vertices.size(); i++) {
+        Node current = g.vertices.at(i);
+        for(int j = 0; j < current.inputs.size(); j++) {
+            cout << current.inputs.at(j) << endl;
+            cout << "Number of outputs: ";
+            cout << visistedOutputs.size() << endl;
+            string inp = current.inputs.at(j); 
+            if (find(visistedOutputs.begin(), visistedOutputs.end(), inp) != visistedOutputs.end()) {
+                cout << "Someone has a dependency" << endl;
+                //found, this means that the input is someone's output
+                //so this current node must be a child of the node with that output
+                current.parent = &visitedNodes.at(j);
+                visitedNodes.at(j).children.push_back(&current);
+            } else {
+                current.parent = &g.noop;
+            }
+        } 
+        visitedNodes.push_back(current);
+        visistedOutputs.push_back(current.output);
+    }
+
     return 0;
 }
